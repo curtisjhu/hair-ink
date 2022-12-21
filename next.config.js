8715b047fdca47e05d5cd8f8887bf388529978f3
Curtis Hu <43627632+curtisjhu@users.noreply.github.com>
2022-12-20 20:48:04 -0800

    fix css?

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? '/hair-ink/' : '',
-  images: {
-    unoptimized: true
-  }
 }
 
 module.exports = nextConfig
